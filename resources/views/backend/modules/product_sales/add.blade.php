58b79fd0f73e265e74c2788ac2849ea293bcdb7c
akazad1235 <akazad914@gmail.com>
2022-02-19 01:07:21 +0600

    product lit and cart add

--- resources/views/backend/modules/product_sales/add.blade.php
+++ resources/views/backend/modules/product_sales/add.blade.php
@@ -5,6 +5,28 @@
              <div class="col-md-5">
                 <div class="card card-primary">
                     <h2>cole one</h2>
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>qty</th>
+                            <th>Price</th>
+                            <th>action</th>
+                        </tr>
+                        </thead>
+                        <tbody id="rows">
+                        </tbody>
+                        <tfoot>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>Total</td>
+                            <td id="total">$0</td>
+                            <td onclick="allPrice()">ok</td>
+                        </tr>
+                        </tfoot>
+                    </table>
                   </div>
              </div>
              <div class="col-md-7">
@@ -45,7 +67,7 @@
                                     </div>
                                   </div>
                             </div>
-                            <div class="col-md-3 product-list">
+                            <div class="col-md-3 product-list"  onclick="sales('monitor2', 300),allPrice()">
                                 <div class="card">
                                     <img class="card-img-top w-100" style="height:100px;border:1px solid red" src="https://static.javatpoint.com/computer/images/mouse.jpg" alt="Card image cap">
                                     <div class="card-body">
@@ -54,11 +76,11 @@
                                     </div>
                                   </div>
                             </div>
-                            <div class="col-md-3 product-list">
+                            <div class="col-md-3 product-list" onclick="sales('monitor', 100), allPrice()">
                                 <div class="card" id="azad">
                                     <img class="card-img-top w-100" style="height:100px;border:1px solid red" src="https://static.javatpoint.com/computer/images/mouse.jpg" alt="Card image cap">
                                     <div class="card-body">
-                                        <h4 class="card-text" id="te">Monitor</h4>
+                                        <h4 class="card-text" id="namess">Monitor</h4>
                                         <p>$100</p>
                                     </div>
                                   </div>
@@ -70,10 +92,10 @@
          </div>
           <!-- /.card -->
 <script>
-   var azad = document.querySelector('#azad');
-   var lists = document.querySelector('#te');
-   console.log(lists.innerText)
-   console.log(lists.getElementsByTagName('h4').innerText)
+ //  var azad = document.querySelector('azad');
+//    var lists = document.querySelector('#te');
+//    console.log(lists.innerText)
+//    console.log(lists.getElementsByTagName('h4').innerText)
    // for (var i=0; i<lists.length; i++){
    //     lists[i].addEventListener('click', function (e){
    //         console.log(lists[i]);
@@ -84,6 +106,66 @@
    //          console.log('ok')
    //     })
    // }
+
+   var row = document.getElementById('rows');
+
+
+var i=1
+
+    function sales(title, price){
+        var newTr = document.createElement('tr');
+        var sl = document.createElement('td');
+        var names = document.createElement('td');
+        var prices = document.createElement('td');
+        prices.setAttribute('class', 'product-price')
+        var qty = document.createElement('input');
+        qty.setAttribute('type','text');
+        qty.setAttribute('size','3');
+        qty.setAttribute('class','quantity');
+        row.appendChild(newTr);
+        newTr.appendChild(sl);
+        sl.innerText = i++;
+        newTr.appendChild(names)
+        names.innerText = title;
+        newTr.appendChild(qty)
+        newTr.appendChild(prices)
+        prices.innerText = price;
+
+
+     //   prices.appendChild(td);
+      //  td.innerText = title;
+        //prices.innerText = price;
+
+    }
+
+
+  function  allPrice(){
+      var sum = 0;
+      var allPrices = document.querySelectorAll('.product-price');
+      for (var i=0; i<allPrices.length; i++){
+          var unitPrice = allPrices[i].textContent;
+          sum = parseInt(unitPrice)+sum
+         // return sum;
+      }
+      document.getElementById('total').innerHTML = sum
+  }
+  function qty(){
+      var quantity = document.querySelectorAll('.quantity');
+      for (var i=0; i<quantity.length; i++){
+          var unitPrice = allPrices[i].value;
+          sum = parseInt(unitPrice)+sum
+          // return sum;
+      }
+  }
+
+
+
+
+
+
+
+
+
 
 </script>
 @endsection
